d0bdb7ea0ff30ee889a380d4c070d317c72e47ff
Juan Lozano <jlozanoa1@unicartagena.edu.co>
2026-09-14 15:20:11 -0500

    Agregado código para el ejercicio 7.

---
+++ src/parteB/Ejercicio7.java
@@ -0,0 +1,9 @@
+public class Ejercicio7 {
+    public static void main(String[] args) {
+        Vehiculo vehiculo1 = new Vehiculo("Toyota", "Picanto", 200);
+        Vehiculo vehiculo2 = new Vehiculo("Mazda", "CX-5", 120);
+
+        vehiculo1.mostrarEstado();
+        vehiculo2.mostrarEstado();
+    }
+}
