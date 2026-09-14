e9930dcebef37b33921481902220459eeb513c29
Juan Lozano <jlozanoa1@unicartagena.edu.co>
2026-09-14 17:56:45 -0500

    Agregado código para el ejercicio 25.
    Se crearon dos objetos de la clase Libro, uno con parámetros y otro sin
    parámetros (valores por defecto).

---
+++ src/parteE/Ejercicio25.java
@@ -0,0 +1,9 @@
+public class Ejercicio25 {
+    public static void main(String[] args) {
+        Libro libro1 = new Libro();
+        Libro libro2 = new Libro("Cien años de soledad", "Gabriel García Márquez", 496);
+
+        libro1.mostrarLibro();
+        libro2.mostrarLibro();
+    }
+}
