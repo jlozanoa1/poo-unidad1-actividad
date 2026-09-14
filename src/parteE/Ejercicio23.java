155529adb62aac2d50168eb86e66eea509b3bad6
Juan Lozano <jlozanoa1@unicartagena.edu.co>
2026-09-14 17:54:21 -0500

    Agregado código para el ejercicio 23.
    Se agregó el constructor por defecto para la clase Libro.

---
+++ src/parteE/Ejercicio23.java
@@ -0,0 +1,7 @@
+public class Ejercicio23 {
+    public static void main(String[] args) {
+        // Libro tiene un constructor vacío que asigna valores por defecto.
+        Libro libro = new Libro();
+        libro.mostrarLibro();
+    }
+}
