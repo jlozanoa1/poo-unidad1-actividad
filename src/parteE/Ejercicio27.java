95bf09d5bff9fbd7bb86ec81b7e5cf6cae504a1e
Juan Lozano <jlozanoa1@unicartagena.edu.co>
2026-09-14 18:00:33 -0500

    Agregado código para el ejercicio 27.
    El constructor de la clase está parametrizado con los parámetros numero,
    titular y saldo inicial.

---
+++ src/parteE/Ejercicio27.java
@@ -0,0 +1,6 @@
+public class Ejercicio27 {
+    public static void main(String[] args) {
+        CuentaBancaria cuenta = new CuentaBancaria("001", "Ana", 500000);
+        cuenta.mostrarCuenta();
+    }
+}
